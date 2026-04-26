9a77d381dc187d80fa09d6b16f18088a57c68179
Carla Cristina <carlacristina130523@gmail.com>
2026-04-26 17:25:19 -0300

    doc: add código no doc da aula

--- conteudo/entendendoJSX.md
+++ conteudo/entendendoJSX.md
@@ -74,3 +74,76 @@ O React (via ESLint/Compilador) monitora o código e emite avisos no console cas
 * Uma variável seja criada mas não utilizada.
 * Tags não sejam fechadas corretamente.
 * Atributos estejam escritos de forma errada (ex: usar `class` em vez de `className`).
+
+
+## Código da Aula:
+```
+import './App.css';
+
+function sum(a, b) {
+  return a + b;
+}
+
+function sub(a, b) {
+  return a - b;
+}
+
+function mult(a, b) {
+  return a * b;
+}
+
+function div(a, b) {
+  return  a /b;
+}
+
+function maior(x) {
+  if(x >= 18) {
+    return 'sim';
+  } else {
+    return 'não';
+  }
+}
+
+const url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSq9FZxZ99-PyL1QmoDJEP1RlUJWc2GA39uFg&s"
+
+function App() {
+  const name = 'Carla'
+  const curso = 'Engenharia de Software'
+  const nameUppercase = name.toLocaleUpperCase();
+  const nameOwercase = name.toLowerCase();
+  const idade = 20;
+
+  return (
+    <div className='App'>
+      <h1>Alterando JSX</h1>
+
+      <section>
+        <h2>Interpolando variáveis</h2>
+        <h2>Sobre mim: </h2>
+        <p>Olá, meu nome é {name}, sou estudante do curso de {curso}</p>  
+        <p>Meu nome em Uppercase: {nameUppercase}</p>
+        <p>Meu nome em OwerCase: {nameOwercase}</p>
+        <p>Sou de maior? {maior(idade)}</p>
+      </section>
+      <section>
+        <h2>Operaçoes e Funções</h2>
+        <p>Soma direta: {10 + 10}</p>
+        <p>Soma pela função: {sum(10, 10)}</p>
+        <p>Subtração direta: {10 - 10}</p>
+        <p>Subtração pela função: {sub(10, 10)}</p>
+        <p>Multiplicação direta: {10 * 10}</p>
+        <p>Multiplicação pela função: {mult(10, 10)}</p>
+        <p>Divisão direta: {10/10}</p>
+        <p>Divisão pela função: {div(10, 10)}</p>
+      </section>
+      <section>
+        <h2>Atributos Dinâmicos: </h2>
+        <p>Gatinho: </p>
+        <img src={url} alt='Gatinho'/>
+      </section>
+    </div>
+  );
+}
+
+
+export default App;
--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,41 +1,69 @@
 import './App.css';
 
-// 1. Boas práticas: Funções utilitárias e constantes estáticas ficam fora do componente.
-// Isso evita processamento desnecessário e vazamento de memória.
 function sum(a, b) {
   return a + b;
 }
 
+function sub(a, b) {
+  return a - b;
+}
+
+function mult(a, b) {
+  return a * b;
+}
+
+function div(a, b) {
+  return  a /b;
+}
+
+function maior(x) {
+  if(x >= 18) {
+    return 'sim';
+  } else {
+    return 'não';
+  }
+}
+
 const url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSq9FZxZ99-PyL1QmoDJEP1RlUJWc2GA39uFg&s"
+
 function App() {
-  // 2. Lógica interna: Variáveis que definem o estado ou comportamento específico deste componente.
-  const name = 'Carla';
-  const nameUpperCase = name.toUpperCase();
+  const name = 'Carla'
+  const curso = 'Engenharia de Software'
+  const nameUppercase = name.toLocaleUpperCase();
+  const nameOwercase = name.toLowerCase();
+  const idade = 20;
 
   return (
-    <div className="App">
-      {/* 3. HTML semântico: Usando hierarquia de títulos e seções para organizar a estrutura da página. */}
-      <h1>Alterando o JSX</h1>
-      
-      <section>
-        <h2>Interpolando Variáveis</h2>
-        <p>Olá, {name}</p>
-        <p>Nome em UpperCase: {nameUpperCase}</p>
-      </section>
+    <div className='App'>
+      <h1>Alterando JSX</h1>
 
       <section>
-        <h2>Operações e Funções</h2>
-        <p>Soma direta no JSX: {2 + 2}</p>
-        <p>Soma via função externa: {sum(1, 2)}</p>
+        <h2>Interpolando variáveis</h2>
+        <h2>Sobre mim: </h2>
+        <p>Olá, meu nome é {name}, sou estudante do curso de {curso}</p>  
+        <p>Meu nome em Uppercase: {nameUppercase}</p>
+        <p>Meu nome em OwerCase: {nameOwercase}</p>
+        <p>Sou de maior? {maior(idade)}</p>
       </section>
-
       <section>
-        <h2>Atributos Dinâmicos</h2>
-        {/* 4. Acessibilidade: O atributo "alt" sempre deve descrever o conteúdo ou função da imagem. */}
-        <img src={url} alt="Amostra de imagem abstrata gerada por base64" />
+        <h2>Operaçoes e Funções</h2>
+        <p>Soma direta: {10 + 10}</p>
+        <p>Soma pela função: {sum(10, 10)}</p>
+        <p>Subtração direta: {10 - 10}</p>
+        <p>Subtração pela função: {sub(10, 10)}</p>
+        <p>Multiplicação direta: {10 * 10}</p>
+        <p>Multiplicação pela função: {mult(10, 10)}</p>
+        <p>Divisão direta: {10/10}</p>
+        <p>Divisão pela função: {div(10, 10)}</p>
+      </section>
+      <section>
+        <h2>Atributos Dinâmicos: </h2>
+        <p>Gatinho: </p>
+        <img src={url} alt='Gatinho'/>
       </section>
     </div>
   );
 }
+
 
 export default App;
